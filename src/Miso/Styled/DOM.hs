4e14b7f1fe39fb32669ec4629edf83b4c956e21d
drryner <drryner@icloud.com>
2020-02-05 04:55:17 +0300

    ADD: Miso.Styled.Tags. UPDATE: Modules structure

---
+++ src/Miso/Styled/DOM.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections     #-}
+
+module Miso.Styled.DOM where
+
+import           Clay                hiding (map)
+import           Data.Coerce         (coerce)
+import qualified Data.HashMap.Strict as HMap
+import           Data.List           (nub)
+import qualified Data.Text           as T
+import qualified Data.Text.Lazy      as TL
+import qualified Miso
+import           Miso.String         (MisoString, ms)
+import qualified Miso.String         as MS
+import           System.IO.Unsafe    (unsafePerformIO)
+import           System.Random
+
+instance Eq Css where
+    s1 == s2 = render s1 == render s2
+    s1 /= s2 = render s1 /= render s2
+
+type View a = VTree a
+
+newtype Attribute a = Attribute (Miso.Attribute a)
+
+data VTree a
+    = VNode MisoString (Maybe T.Text) (Maybe Css) [Attribute a] [VTree a]
+    | VText MisoString
+
+el :: MisoString -> [Miso.Attribute a] -> [View a] -> View a
+el tag attrs = VNode tag Nothing Nothing (coerce attrs)
+
+generateHtml :: HMap.HashMap TL.Text Int -> MisoString -> View a -> Miso.View a
+generateHtml _ _ (VText str) = Miso.text str
+generateHtml cssHash uniqId (VNode tag mbClasses (Just css) attrs childs) = Miso.nodeHtml
+    tag
+    (coerce attrs ++ case HMap.lookup (render css) cssHash of
+        Just className ->
+            [ Miso.class_ $ mconcat
+                [ "_" <> uniqId <> Miso.String.ms className
+                , maybe "" ((" " <>) . Miso.String.ms) mbClasses
+                ]
+            ]
+        Nothing        -> []
+    )
+    $ map (generateHtml cssHash uniqId) childs
+generateHtml cssHash uniqId (VNode tag _ Nothing attrs childs) = Miso.nodeHtml
+    tag
+    (coerce attrs)
+    $ map (generateHtml cssHash uniqId) childs
+
+collectCss :: View a -> [Css]
+collectCss (VText _                    ) = mempty
+collectCss (VNode _ _ (Just css) _ childs) = css : mconcat (map collectCss childs)
+collectCss (VNode _ _ Nothing    _ childs) = mconcat (map collectCss childs)
+
+{-# NOINLINE rnd #-}
+rnd :: () -> Int
+rnd _ = unsafePerformIO $ randomRIO (0, 2000000000)
+
+toUnstyled :: View a -> Miso.View a
+toUnstyled tree = Miso.div_ []
+    [ Miso.nodeHtml "style" [] [ Miso.text $ Miso.String.ms $ mconcat $ map render renderCss ]
+    , generateHtml cssHash (Miso.String.ms uniqId) tree
+    ]
+  where
+    uniqId    = T.pack $ show $ rnd ()
+    renderCss = map (\(css, id') -> element ("._" <> uniqId <> T.pack (show id')) ? css) cssKeyed
+    cssHash   = HMap.fromList (map (\(css, id') -> (render css, id')) cssKeyed)
+    cssKeyed  = map (, rnd ()) css
+    css       = nub $ collectCss tree
+
+toUnstyled' :: View a -> [Miso.View a]
+toUnstyled' tree = [ Miso.nodeHtml "style" [] [ Miso.text $ Miso.String.ms $ mconcat $ map render renderCss ]
+                   , generateHtml cssHash (Miso.String.ms uniqId) tree
+                   ]
+  where
+    uniqId    = T.pack $ show $ rnd ()
+    renderCss = map (\(css, id') -> element ("._" <> uniqId <> T.pack (show id')) ? css) cssKeyed
+    cssHash   = HMap.fromList (map (\(css, id') -> (render css, id')) cssKeyed)
+    cssKeyed  = map (, rnd ()) css
+    css       = nub $ collectCss tree
+
+text :: MisoString -> VTree a
+text = VText
+
+styled :: MisoString -> Css -> [Miso.Attribute a] -> [View a] -> View a
+styled tag css attrs = VNode tag Nothing (Just css) (coerce attrs)
+
+styled' :: MisoString -> Css -> MisoString -> [Miso.Attribute a] -> [View a] -> View a
+styled' tag css classes attrs = VNode tag (Just $ T.pack $ MS.unpack classes) (Just css) (coerce attrs)
+
+linkCss :: MisoString -> View a
+linkCss path = el "link" [ Miso.rel_ "stylesheet", Miso.href_ path ] []
---
+++ tags.hs
@@ -0,0 +1,300 @@
+a_ :: Tag event
+a_ = el "a"
+
+abbr_ :: Tag event
+abbr_ = el "abbr"
+
+address_ :: Tag event
+address_ = el "address"
+
+area_ :: Tag event
+area_ = el "area"
+
+article_ :: Tag event
+article_ = el "article"
+
+aside_ :: Tag event
+aside_ = el "aside"
+
+audio_ :: Tag event
+audio_ = el "audio"
+
+b_ :: Tag event
+b_ = el "b"
+
+base_ :: Tag event
+base_ = el "base"
+
+bdo_ :: Tag event
+bdo_ = el "bdo"
+
+blockquote_ :: Tag event
+blockquote_ = el "blockquote"
+
+body_ :: Tag event
+body_ = el "body"
+
+br_ :: Tag event
+br_ = el "br"
+
+button_ :: Tag event
+button_ = el "button"
+
+canvas_ :: Tag event
+canvas_ = el "canvas"
+
+caption_ :: Tag event
+caption_ = el "caption"
+
+cite_ :: Tag event
+cite_ = el "cite"
+
+code_ :: Tag event
+code_ = el "code"
+
+col_ :: Tag event
+col_ = el "col"
+
+colgroup_ :: Tag event
+colgroup_ = el "colgroup"
+
+command_ :: Tag event
+command_ = el "command"
+
+datalist_ :: Tag event
+datalist_ = el "datalist"
+
+dd_ :: Tag event
+dd_ = el "dd"
+
+del_ :: Tag event
+del_ = el "del"
+
+details_ :: Tag event
+details_ = el "details"
+
+dfn_ :: Tag event
+dfn_ = el "dfn"
+
+div_ :: Tag event
+div_ = el "div"
+
+dl_ :: Tag event
+dl_ = el "dl"
+
+dt_ :: Tag event
+dt_ = el "dt"
+
+em_ :: Tag event
+em_ = el "em"
+
+embed_ :: Tag event
+embed_ = el "embed"
+
+fieldset_ :: Tag event
+fieldset_ = el "fieldset"
+
+figcaption_ :: Tag event
+figcaption_ = el "figcaption"
+
+figure_ :: Tag event
+figure_ = el "figure"
+
+footer_ :: Tag event
+footer_ = el "footer"
+
+form_ :: Tag event
+form_ = el "form"
+
+h1_ :: Tag event
+h1_ = el "h1"
+
+h2_ :: Tag event
+h2_ = el "h2"
+
+h3_ :: Tag event
+h3_ = el "h3"
+
+h4_ :: Tag event
+h4_ = el "h4"
+
+h5_ :: Tag event
+h5_ = el "h5"
+
+h6_ :: Tag event
+h6_ = el "h6"
+
+head_ :: Tag event
+head_ = el "head"
+
+header_ :: Tag event
+header_ = el "header"
+
+hr_ :: Tag event
+hr_ = el "hr"
+
+html_ :: Tag event
+html_ = el "html"
+
+i_ :: Tag event
+i_ = el "i"
+
+iframe_ :: Tag event
+iframe_ = el "iframe"
+
+img_ :: Tag event
+img_ = el "img"
+
+input_ :: Tag event
+input_ = el "input"
+
+ins_ :: Tag event
+ins_ = el "ins"
+
+kdb_ :: Tag event
+kdb_ = el "kdb"
+
+label_ :: Tag event
+label_ = el "label"
+
+li_ :: Tag event
+li_ = el "li"
+
+link_ :: Tag event
+link_ = el "link"
+
+map_ :: Tag event
+map_ = el "map"
+
+main_ :: Tag event
+main_ = el "main"
+
+menu_ :: Tag event
+menu_ = el "menu"
+
+menuitem_ :: Tag event
+menuitem_ = el "menuitem"
+
+meta_ :: Tag event
+meta_ = el "meta"
+
+nav_ :: Tag event
+nav_ = el "nav"
+
+noscript_ :: Tag event
+noscript_ = el "noscript"
+
+object_ :: Tag event
+object_ = el "object"
+
+ol_ :: Tag event
+ol_ = el "ol"
+
+option_ :: Tag event
+option_ = el "option"
+
+p_ :: Tag event
+p_ = el "p"
+
+param_ :: Tag event
+param_ = el "param"
+
+svg_ :: Tag event
+svg_ = el "svg"
+
+pre_ :: Tag event
+pre_ = el "pre"
+
+progress_ :: Tag event
+progress_ = el "progress"
+
+q_ :: Tag event
+q_ = el "q"
+
+rp_ :: Tag event
+rp_ = el "rp"
+
+rt_ :: Tag event
+rt_ = el "rt"
+
+script_ :: Tag event
+script_ = el "script"
+
+section_ :: Tag event
+section_ = el "section"
+
+select_ :: Tag event
+select_ = el "select"
+
+small_ :: Tag event
+small_ = el "small"
+
+source_ :: Tag event
+source_ = el "source"
+
+span_ :: Tag event
+span_ = el "span"
+
+strong_ :: Tag event
+strong_ = el "strong"
+
+style_ :: Tag event
+style_ = el "style"
+
+sub_ :: Tag event
+sub_ = el "sub"
+
+summary_ :: Tag event
+summary_ = el "summary"
+
+table_ :: Tag event
+table_ = el "table"
+
+sup_ :: Tag event
+sup_ = el "sup"
+
+tbody_ :: Tag event
+tbody_ = el "tbody"
+
+td_ :: Tag event
+td_ = el "td"
+
+textarea_ :: Tag event
+textarea_ = el "textarea"
+
+tfoot_ :: Tag event
+tfoot_ = el "tfoot"
+
+th_ :: Tag event
+th_ = el "th"
+
+template_ :: Tag event
+template_ = el "template"
+
+thread_ :: Tag event
+thread_ = el "thread"
+
+time_ :: Tag event
+time_ = el "time"
+
+title_ :: Tag event
+title_ = el "title"
+
+tr_ :: Tag event
+tr_ = el "tr"
+
+track_ :: Tag event
+track_ = el "track"
+
+ul_ :: Tag event
+ul_ = el "ul"
+
+var_ :: Tag event
+var_ = el "var"
+
+video_ :: Tag event
+video_ = el "video"
+
+wbr_ :: Tag event
+wbr_ = el "wbr"
+
